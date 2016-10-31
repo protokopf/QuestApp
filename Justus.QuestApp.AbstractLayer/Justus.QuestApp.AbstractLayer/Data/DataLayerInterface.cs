5560d049b8d88b29036fdf6ee275dea09d9f2323
Константин <@DESKTOP-R1A1S1O>
2016-10-31 20:10:20 +0200

    - Add DL interface.

---
+++ Justus.QuestApp.AbstractLayer/Justus.QuestApp.AbstractLayer/Data/DataLayerInterface.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Justus.QuestApp.AbstractLayer.Entities;
+
+namespace Justus.QuestApp.AbstractLayer.Data
+{
+    /// <summary>
+    /// Interface to data layer.
+    /// </summary>
+    public interface IDataLayerInterface<TEntity> : IDisposable where TEntity : IdentifiedEntity
+    {
+        /// <summary>
+        /// Opens connection to data storage.
+        /// </summary>
+        void Open();
+
+        /// <summary>
+        /// Closes connection to data storage.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Saves entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        void Save(TEntity entity);
+
+        /// <summary>
+        /// Saves all entities.
+        /// </summary>
+        /// <param name="entities"></param>
+        void SaveAll(List<TEntity> entities);
+
+        /// <summary>
+        /// Returns entity by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        TEntity Get(int id);
+
+        /// <summary>
+        /// Returns all entities from data storage.
+        /// </summary>
+        /// <returns></returns>
+        List<TEntity> GetAll();
+    }
+}

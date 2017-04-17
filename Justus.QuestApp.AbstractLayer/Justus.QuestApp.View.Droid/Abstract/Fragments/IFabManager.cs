4c6ee3c401219263c57a02676e6286883d26a40e
Konstantin <protokopf@gmail.com>
2017-04-17 17:05:23 +0300

    - Added IFabManager intefrace.

---
+++ Justus.QuestApp.AbstractLayer/Justus.QuestApp.View.Droid/Abstract/Fragments/IFabManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace Justus.QuestApp.View.Droid.Abstract.Fragments
+{
+    /// <summary>
+    /// Represents interface for types, which manages FAB.
+    /// </summary>
+    public interface IFabManager
+    {
+        /// <summary>
+        /// Manages given FAB.
+        /// </summary>
+        /// <param name="fab"></param>
+        void Manage(FloatingActionButton fab);
+    }
+}
